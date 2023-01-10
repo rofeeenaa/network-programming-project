1c6f1fe37d1a8e4f5ae8cba5c508aed3823510d8
unknown <rofeena80@gmail.com>
2023-01-10 21:23:10 +0500

    initial commit

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { HeartTwoTone } from "@ant-design/icons";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+    
+    </footer>
+  );
+};
+
+export default Footer;
